a7168031ab122d308a54531b838200c832674fb0
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-12-17 01:35:49 -0800

    create db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,6 +3,12 @@
 from sqlalchemy import create_engine
 from models.base_model import Base
 from os import getenv
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 class DBStorage:
@@ -20,3 +26,41 @@ class DBStorage:
                                       pool_pre_ping=True)
         if getenv('HBNB_ENV') == "test":
             Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """query on the current database session (self.__session)
+        all objects depending of the class name (argument cls)
+        """
+        classes = [State, City, User, Place, Review, Amenity]
+        objects = []
+        if cls is None:
+            for item in classes:
+                objects.extend(self.__session.query(item).all())
+        else:
+            objects = self.__session.query(cls)
+        dict_ = {}
+        for obj in objects:
+            dict_[f"{type(obj).__name__}.{obj.id}"] = obj
+        return dict_
+
+    def new(self, obj):
+        """add the object to the current database session (self.__session)"""
+        self.__session.add(obj)
+
+    def save(self):
+        """commit all changes of the current database session (self.__session)
+        """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """delete from the current database session obj if not None"""
+        if obj is not None:
+            self.__session.delete()
+
+    def reload(self):
+        """create all tables in the database
+        create the current database session (self.__session) from the engine"""
+        Base.metadata.create_all(self.__engine)
+        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(Session)
+        self.__session = Session()
