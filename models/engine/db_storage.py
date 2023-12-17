156ee909d10adfaf7c7f64b015ed0021ed46378c
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-12-16 21:22:57 -0800

    connect to the db

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -12,11 +12,11 @@ class DBStorage:
 
     def __init__(self):
         """create the engine"""
-        self.engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                    .format(getenv('HBNB_MYSQL_USER'),
-                                            getenv('HBNB_MYSQL_PWD'),
-                                            getenv('HBNB_MYSQL_HOST'),
-                                            getenv('HBNB_MYSQL_DB')),
-                                    pool_pre_ping=True)
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(getenv('HBNB_MYSQL_USER'),
+                                              getenv('HBNB_MYSQL_PWD'),
+                                              getenv('HBNB_MYSQL_HOST'),
+                                              getenv('HBNB_MYSQL_DB')),
+                                      pool_pre_ping=True)
         if getenv('HBNB_ENV') == "test":
             Base.metadata.drop_all(self.__engine)
