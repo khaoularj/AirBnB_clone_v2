5ec3fc062dadad992987ec43cf6b2e451b540f6d
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-12-16 21:21:39 -0800

    connect to the db

---
+++ models/engine/db_storage.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This module defines a class to manage database storage for hbnb clone"""
+from sqlalchemy import create_engine
+from models.base_model import Base
+from os import getenv
+
+
+class DBStorage:
+    """This class manages storage of hbnb models in JSON format"""
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """create the engine"""
+        self.engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                    .format(getenv('HBNB_MYSQL_USER'),
+                                            getenv('HBNB_MYSQL_PWD'),
+                                            getenv('HBNB_MYSQL_HOST'),
+                                            getenv('HBNB_MYSQL_DB')),
+                                    pool_pre_ping=True)
+        if getenv('HBNB_ENV') == "test":
+            Base.metadata.drop_all(self.__engine)
