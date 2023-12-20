9bef0c312e79e3b8f4e9cc39aba732ab9322ad9d
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-12-19 23:23:51 -0800

    fix bugs

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage or DBStorage"""
 from os import getenv
-
+from models.state import State
+from models.review import Review
+from models.amenity import Amenity
+from models.user import User
+from models.place import Place
+from models.city import City
 
 if getenv("HBNB_TYPE_STORAGE") == "db":
     from models.engine.db_storage import DBStorage
