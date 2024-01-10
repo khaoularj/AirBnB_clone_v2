2f039441abcb3c325d320ce1da1e29cb95c4caf9
khaoularj <khaoula.raja1@gmail.com>
2024-01-10 07:39:32 -0800

    Prepare your web servers

---
+++ web_static/0-setup_web_static.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#this script sets up the web servers for the deployment of web_static
+
+sudo apt-get update
+sudo apt-get -y install nginx
+
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+
+sudo echo "<html><head></head><body>This is a simple test</body></html>" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+#sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server/a\    location \/hbnb_static {\n        alias \/data\/web_static\/current\/;\n    }' /etc/nginx/sites-available/default
+
+sudo service nginx restart
