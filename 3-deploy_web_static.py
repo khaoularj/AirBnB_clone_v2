159da11d90dc0266c84bd40604646e82c61c328a
khaoularj <khaoula.raja1@gmail.com>
2024-01-11 03:56:43 -0800

    Full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-#this script creates and distributes an archive to the
-#web servers, using the function deploy
+# this script creates and distributes an archive to the
+# web servers, using the function deploy
 from pack_web_static import do_pack
 from do_deploy_web_static import do_deploy
 
