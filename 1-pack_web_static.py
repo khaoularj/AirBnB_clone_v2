0934f4eb6adaf90555b5050bbcccf45938b6ce0e
khaoularj <khaoula.raja1@gmail.com>
2024-01-11 01:44:44 -0800

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,35 +1,33 @@
 #!/usr/bin/python3
-"""Fabric script that generates a .tgz archive 
-from the contents of the web_static folder"""
+"""this script generates a .tgz archive 
+from the contents of the web_static folder of the 
+AirBnB Clone repo, using the function do_pack"""
+
 from fabric.api import local
 from datetime import datetime
 import os
 
 
 def do_pack():
-    """Generates a .tgz archive from the contents of the web_static folder"""
+    """this function generates a .tgz archive"""
 
-    # Create the versions folder if it doesn't exist
     if not os.path.exists("versions"):
         local("mkdir -p versions")
 
-    # Generate the name of the archive
-    time_format = "%Y%m%d%H%M%S"
-    archive_name = "web_static_{}.tgz".format(datetime.utcnow().strftime(time_format))
+    time = "%Y%m%d%H%M%S"
+    arch_name = "web_static_{}.tgz".format(datetime.utcnow().strftime(time))
 
-    # Compress the contents of the web_static folder
-    command = "tar -cvzf versions/{} web_static".format(archive_name)
+    print("Packing web_static to versions/{}".format(arch_name))
+
+    command = "tar -cvzf versions/{} web_static".format(arch_name)
     result = local(command)
 
-    # Check if the archive was created successfully
     if result.failed:
         return None
     else:
-        return "versions/{}".format(archive_name)
+        return "versions/{}".format(arch_name)
 
 if __name__ == "__main__":
     result = do_pack()
     if result:
         print("web_static packed: {} -> {}Bytes".format(result, os.path.getsize(result)))
-    else:
-        print("Packing web_static failed.")
