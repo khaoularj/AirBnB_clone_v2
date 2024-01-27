0376ce4b98a7f9e257b416395ed29a896c7268b0
khaoularj <khaoula.raja1@gmail.com>
2024-01-27 13:30:16 -0800

    adding /c/<text>

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""this script starts a Flask web app"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """function that display HELLO HBNB"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """function that display HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C(text):
+    """function that display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
